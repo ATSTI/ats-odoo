dbcb936ab8e5ccff4f96aaf3ad3ca73841711657
Carlos <ats@atsti.com.br>
2022-10-05 15:01:38 -0300

    Modulo de Email pra Fatura

---
+++ mail_invoice_cron/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 Carlos - ATSti Soluções
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Send mail invoice cron",
+    "version": "14.0.0.0.0",
+    "development_status": "Alfa",
+    "category": "Invoice",
+    "summary": "Mail invoice cron",
+    "author": "ATSti",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+        ],
+    "data": [
+        # "security/ir.model.access.csv",
+    ],
+    "installable": True,
+    "auto_install": False
+}

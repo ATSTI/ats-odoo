19cfbcad060e2bcf12e559b3aa9fdf5f20b4176a
Carlos Silveira <crsilveira@gmail.com>
2023-01-19 10:41:34 -0300

    correção do texto

--- maintenance_picking/__manifest__.py
+++ maintenance_picking/__manifest__.py
@@ -5,8 +5,9 @@
     "version": "14.0.1.1.0",
     "category": "maintenance",
     "description" : "Cria retorno para o estoque quando a OM e concluida. \
-        IMPORTANTE: necessario criar um tipo de operação no inventário: \
-               Manutenção, com local origem Manutenção e destino Stock",
+        IMPORTANTE: necessario criar um tipo de operação \
+            no inventário: Manutenção \
+            com local origem Manutenção e destino Stock",
     "author": "Mauricio, ATSTi Soluções",
     "website": "",
     "license": "LGPL-3",

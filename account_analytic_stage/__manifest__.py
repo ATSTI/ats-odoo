6a78535db7046bb8e136b91fe8f0fca8567a3edc
Mauricio-ATS <maurs320@gmail.com>
2023-02-22 16:29:38 -0300

    Estagios, E data na conta

---
+++ account_analytic_stage/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2017 ACSONE SA/NV
+# Copyright 2020 Brainbean Apps (https://brainbeanapps.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Analytic Sequence",
+    "summary": """
+        Restore the analytic account sequence""",
+    "version": "14.0.1.0.1",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-analytic",
+    "depends": [
+        "analytic",
+    ],
+    "data": [
+        "views/stage.xml",
+    ],
+}
---
+++ account_analytic_stage/models/account_analytic_stage.py
@@ -0,0 +1,34 @@
+# Copyright 2017 ACSONE SA/NV
+# Copyright 2020 Brainbean Apps (https://brainbeanapps.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class AccountAnalyticAccount(models.Model):
+    _inherit = "account.analytic.account"
+
+    date_start = fields.Date('Data de Inicio:')
+    date_end = fields.Date('Data Final:')
+    state_id = fields.Selection([
+            ('andamento', 'Andamento'),
+            ('concluido', 'Concluido'),
+            ('nao_iniciado', 'Não iniciado'),
+        ],
+        string="Estágio"
+    )
+
+    # @api.model
+    # def create(self, vals):
+    #     if "code" not in vals:
+    #         vals["code"] = self._default_code()
+    #     return super().create(vals)
+
+    # @api.model
+    # def _default_code(self):
+    #     return self.env["ir.sequence"].next_by_code("account.analytic.account.code")
+
+    # @api.model
+    # def _assign_default_codes(self):
+    #     for aaa in self.with_context(active_test=False).search([("code", "=", False)]):
+    #         aaa.code = self._default_code()

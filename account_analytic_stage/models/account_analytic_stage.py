26982fbceaec6cbe3335b4710e5c80d5e1478f71
Carlos Silveira <crsilveira@gmail.com>
2023-02-22 17:38:32 -0300

    ajuste models

--- account_analytic_stage/models/account_analytic_stage.py
+++ account_analytic_stage/models/account_analytic_stage.py
@@ -1,5 +1,3 @@
-# Copyright 2017 ACSONE SA/NV
-# Copyright 2020 Brainbean Apps (https://brainbeanapps.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models

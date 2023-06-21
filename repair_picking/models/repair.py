0e8d0e7dd1d14d475e8cf7dc5ca93ebb0c8631e1
Carlos Silveira <crsilveira@gmail.com>
2023-06-21 09:09:43 -0300

    adicionado import.

--- repair_picking/models/repair.py
+++ repair_picking/models/repair.py
@@ -1,10 +1,8 @@
 # © 2018  Carlos R. Silveira
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from ast import literal_eval
-from odoo import api, fields, models, _
-from datetime import datetime
-from odoo.exceptions import UserError, ValidationError
+from odoo import models, _
+from odoo.tools import float_compare
 
 
 class RepairOrder(models.Model):

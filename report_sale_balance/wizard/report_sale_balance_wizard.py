b1d1ee95979b3b06b2ad6f3781580aef2be58f71
Carlos Silveira <crsilveira@gmail.com>
2023-07-27 11:49:09 -0300

    codigo refatorado.

--- report_sale_balance/wizard/report_sale_balance_wizard.py
+++ report_sale_balance/wizard/report_sale_balance_wizard.py
@@ -1,7 +1,5 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 from odoo import api, fields, models
-
 
 class ReportSaleBalanceWizard(models.TransientModel):
     _name = "report.sale.balance.wizard"
@@ -10,27 +8,7 @@ class ReportSaleBalanceWizard(models.TransientModel):
     date_start = fields.Date('Data Inicial')
     date_end = fields.Date('Data Final')
 
-    # def _get_report_values(self, docids, data=None):
-    #     import pudb;pu.db
-    #     docs = self.env['sale.order'].search([("date_order", ">=", self.date_start),("date_order", "<=", self.date_end)])
-    #     return {
-    #         'doc_ids': docids,
-    #           'doc_model': 'sale.order',
-    #           'docs': docs,
-    #           'data': data,
-    #     }
-
     def action_report_balance(self):
-        # docs = self.env['sale.order'].search([("date_order", ">=", self.date_start),("date_order", "<=", self.date_end)])
-        # return {
-        #     'type': 'ir.actions.act_window',
-        #     'view_mode': 'form',
-        #     'res_model': 'sale.order',
-        #     'docs': docs,
-        #     'views': [(False, 'form')],
-        #     'context': '',
-        #     'view_id': False,
-        # }
         data = {
             'date_start': self.date_start,
             'date_end': self.date_end,
@@ -38,9 +16,4 @@ class ReportSaleBalanceWizard(models.TransientModel):
         docargs = {
             'data': data,
         }
-            # 'doc_ids': docs.ids,
-            # 'doc_model': 'sale.order',
-            # 'docs': docs,
-
-        # import pudb;pu.db
         return self.env.ref('report_sale_balance.action_report_sale_balance').report_action(None, data=docargs)

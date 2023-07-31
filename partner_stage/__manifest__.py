7afe702206dc0fec59d8820aa33598d6659c1c54
Carlos Silveira <crsilveira@gmail.com>
2023-07-31 11:37:25 -0300

    Adicionando Colunas no Kanban Partner

---
+++ partner_stage/__manifest__.py
@@ -0,0 +1,25 @@
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'Estágio Kanban Partner',
+    'description': """""",
+    'version': '1.0',
+    'category': 'Partner',
+    'author': 'ATSTi',
+    'website': 'http://www.atsti.com.br',
+    'license': 'AGPL-3',
+    'contributors': [
+        'Carlos Silveira<carlos@atsti.com.br>',
+    ],
+    'depends': [
+        'base'
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/partner_stage_data.xml',
+        'views/partner_stage.xml',
+        'views/partner_view.xml',
+    ],
+    'demo': [],
+    'installable': True,
+}

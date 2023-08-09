2cd9d380a2d98d95096135e13a5b09915aead497
Carlos Silveira <crsilveira@gmail.com>
2023-08-09 17:20:41 -0300

    mostrar qdo tem somente compra tbem, e correcao horario

--- report_sale_balance/report/report_sale_balance.py
+++ report_sale_balance/report/report_sale_balance.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
+from datetime import datetime, timedelta
 from odoo import fields, api, models, _, tools
 
 
@@ -9,35 +10,49 @@ class BalanceReport(models.AbstractModel):
     _description = 'Relatorio Vendas/Compras'
 
     def _get_report_values(self, docids, data=None):
+        date_start = datetime.strptime(
+            '%s 00:01:00' %(data['data']['date_start']), "%Y-%m-%d %H:%M:%S") + timedelta(hours=3)
+        # date_start = fields.Datetime.context_timestamp(self, document_date)
+        date_end = datetime.strptime(
+            '%s 23:59:00' %(data['data']['date_end']), "%Y-%m-%d %H:%M:%S") + timedelta(hours=3)
+        # date_end = fields.Datetime.context_timestamp(self, document_date)
         t_out = self.env['sale.order'].search([
-            ("commitment_date", ">=", data['data']['date_start']),
-            ("commitment_date", "<=", data['data']['date_end'])])
+            ("commitment_date", ">=", date_start),
+            ("commitment_date", "<=", date_end)])
         t_in = self.env['purchase.order'].search([
-            ("date_planned", ">=", data['data']['date_start']),
-            ("date_planned", "<=", data['data']['date_end'])])
-        prod_ids = t_out.order_line.mapped("product_id")
+            ("date_planned", ">=", date_start),
+            ("date_planned", "<=", date_end)])
+        
+        prod_out = t_out.order_line.mapped("product_id")
+        prod_in = t_out.order_line.mapped("product_id")
+        prod_ids = set()
+        for p in prod_out.ids:
+            prod_ids.add(p)
+        for p in prod_in.ids:
+            prod_ids.add(p)
         itens = []
-        for pr in prod_ids:
+        for pr in list(prod_ids):
             vals = {}
+            prod = self.env['product.product'].browse([pr])
             p_out = sum(
                 line.product_uom_qty for line in t_out.order_line.filtered(
-                    lambda x: x.product_id.id in [pr.id]
+                    lambda x: x.product_id.id in [prod.id]
                )
             )
             p_in = sum(
                 line.product_uom_qty for line in t_in.order_line.filtered(
-                    lambda x: x.product_id.id in [pr.id]
+                    lambda x: x.product_id.id in [prod.id]
                )
             )
             d = data['data']['date_start']
             date_in = f"{d[8:10]}/{d[5:7]}/{d[:4]}"
             d = data['data']['date_end']
             date_end =  f"{d[8:10]}/{d[5:7]}/{d[:4]}"
-            vals['product_id'] = pr.id
-            vals['name'] = f"[{pr.default_code}]{pr.name}"
-            vals['prod'] = pr.name
-            vals['preco'] = pr.lst_price
-            vals['unidade'] = pr.uom_id.code
+            vals['product_id'] = prod.id
+            vals['name'] = f"[{prod.default_code}]{prod.name}"
+            vals['prod'] = prod.name
+            vals['preco'] = prod.lst_price
+            vals['unidade'] = prod.uom_id.code
             vals['venda'] = p_out
             vals['compra'] = p_in
             vals['saldo'] = p_in - p_out

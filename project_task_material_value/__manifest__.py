74c955418a57d5a860d28ce072367ec839b6aa3f
Mauricio-ATS <maurs320@gmail.com>
2023-10-06 18:08:16 -0300

    project_task_material_value

---
+++ project_task_material_value/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2023 - Mauricio Silveira
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3.0).
+{
+    "name": "Project Task Material Value",
+    "summary": "Da valor á linha de materiais",
+    "version": "14.0.1.0.0",
+    "category": "Project Management",
+    "author": "Mauricio Silveira," "ATSti," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/project",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["project", "product"],
+    "data": ["security/ir.model.access.csv", "views/project_view.xml"],
+}

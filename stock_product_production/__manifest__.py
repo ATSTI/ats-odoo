d4a880b28e62caf0ffa5142531aa93b9ae8a4649
Carlos Silveira <crsilveira@gmail.com>
2023-11-09 16:12:31 -0300

    Permite o usuário normal editar local de producao no cad. produto.

---
+++ stock_product_production/__manifest__.py
@@ -0,0 +1,15 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Strock Product Production ",
+    "summary": "Remove grupo_no_one do cadastro do produto \
+        , mostrando o campo local producao para todos usuarios",
+    "author": "ATSTi",
+    "website": "",
+    "category": "Stock",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["stock"],
+    "data": ["views/product_views.xml"],
+    "installable": True,
+}

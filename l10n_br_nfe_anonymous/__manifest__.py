553d043d62a4424c4d4a317b8acf311fc893c55d
Carlos Silveira <crsilveira@gmail.com>
2023-11-17 14:49:36 -0300

    Adicionado o campo anonymous_consumer para NFCe sem pos

---
+++ l10n_br_nfe_anonymous/__manifest__.py
@@ -0,0 +1,22 @@
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "NF-e",
+    "summary": "Brazilian Eletronic Invoice NF-e / NFC-e Is Anonymous Consumer ",
+    "category": "Localisation",
+    "license": "AGPL-3",
+    "author": "ATSTi",
+    "website": "",
+    "development_status": "Beta",
+    "version": "14.0.1.0.0",
+    "depends": [
+        "l10n_br_nfe",
+    ],
+    "data": [
+        "views/res_partner.xml",
+    ],
+    "demo": [
+    ],
+    "installable": True,
+    "auto_install": False,
+}

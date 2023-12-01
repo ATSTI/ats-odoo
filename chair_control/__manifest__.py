1ba15863d639e38600d1edbcbf04878cd73f8952
Mauricio-ATS <maurs320@gmail.com>
2023-12-01 17:25:28 -0300

    incopleto: migracao_chair

---
+++ chair_control/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+
+
+{
+    'name': 'Controle de cadeiras',
+    'version': '1.0',
+    'category': 'Others',
+    'sequence': 2,
+    'summary': 'controle de cadeiras',
+    'description': """
+    """,
+    'author': 'otaviosilmunhoz@hotmail.com/ATS Soluções',
+    'website': '',
+    'depends': ['event_sale'],
+    'data': [#'views/chair_view.xml',
+             'views/event_view.xml',
+            #  'report/chair_template.xml',
+            #  'report/event_template.xml',
+            'security/ir.model.access.csv'
+    ],
+    'installable': True,
+    'application': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:

588fc2609d685cd29cdf9a853269f66193034f80
Carlos Silveira <crsilveira@gmail.com>
2024-03-04 11:16:52 -0300

    adicionando tipo de venda no relatorio pos

---
+++ pos_report_order/models/pos_order_report.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, tools
+
+
+class PosOrderReport(models.Model):
+    _inherit = "report.pos.order"
+
+    tipo_venda = fields.Char('Tipo Venda')
+
+    def _select(self):
+        return """
+            SELECT
+                MIN(l.id) AS id,
+                COUNT(*) AS nbr_lines,
+                s.date_order AS date,
+                SUM(l.qty) AS product_qty,
+                SUM(l.qty * l.price_unit / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) AS price_sub_total,
+                SUM(ROUND((l.qty * l.price_unit) * (100 - l.discount) / 100 / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END, cu.decimal_places)) AS price_total,
+                SUM((l.qty * l.price_unit) * (l.discount / 100) / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END) AS total_discount,
+                CASE
+                    WHEN SUM(l.qty * u.factor) = 0 THEN NULL
+                    ELSE (SUM(l.qty*l.price_unit / CASE COALESCE(s.currency_rate, 0) WHEN 0 THEN 1.0 ELSE s.currency_rate END)/SUM(l.qty * u.factor))::decimal
+                END AS average_price,
+                SUM(cast(to_char(date_trunc('day',s.date_order) - date_trunc('day',s.create_date),'DD') AS INT)) AS delay_validation,
+                s.id as order_id,
+                s.partner_id AS partner_id,
+                s.state AS state,
+                s.user_id AS user_id,
+                s.company_id AS company_id,
+                s.sale_journal AS journal_id,
+                l.product_id AS product_id,
+                pt.categ_id AS product_categ_id,
+                p.product_tmpl_id,
+                ps.config_id,
+                pt.pos_categ_id,
+                s.pricelist_id,
+                s.session_id,
+                s.account_move IS NOT NULL AS invoiced
+        """
+
+    def _from(self):
+        return """
+            FROM pos_order_line AS l
+                INNER JOIN pos_order s ON (s.id=l.order_id)
+                LEFT JOIN product_product p ON (l.product_id=p.id)
+                LEFT JOIN product_template pt ON (p.product_tmpl_id=pt.id)
+                LEFT JOIN uom_uom u ON (u.id=pt.uom_id)
+                LEFT JOIN pos_session ps ON (s.session_id=ps.id)
+                LEFT JOIN res_company co ON (s.company_id=co.id)
+                LEFT JOIN res_currency cu ON (co.currency_id=cu.id)
+        """
+
+    def _group_by(self):
+        return """
+            GROUP BY
+                s.id, s.date_order, s.partner_id,s.state, pt.categ_id,
+                s.user_id, s.company_id, s.sale_journal,
+                s.pricelist_id, s.account_move, s.create_date, s.session_id,
+                l.product_id,
+                pt.categ_id, pt.pos_categ_id,
+                p.product_tmpl_id,
+                ps.config_id
+        """
+
+    def init(self):
+        tools.drop_view_if_exists(self._cr, self._table)
+        self._cr.execute("""
+            CREATE OR REPLACE VIEW %s AS (
+                %s
+                %s
+                %s
+            )
+        """ % (self._table, self._select(), self._from(), self._group_by())
+        )

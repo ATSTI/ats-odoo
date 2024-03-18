2dc56e192651ca38bb4244e0d6ee7bf7e67c3f8b
Carlos Silveira <crsilveira@gmail.com>
2024-03-18 17:07:23 -0300

    Enviando mensagem para notificar usuario do vencimento certificado

--- reminder_certificate_cron/models/reminder_certificate_cron.py
+++ reminder_certificate_cron/models/reminder_certificate_cron.py
@@ -9,21 +9,30 @@ class ReminderCertificateCron(models.Model):
     _inherit = ['mail.thread']
     _description = "Lembrete de vencimento do certificado"
 
-    def cron_lembrete_certificate(self, dia_vcto=0):
+    def cron_lembrete_certificate(self, dia_vcto=0, intervalo=5):
         cert_obj = self.env['l10n_br_fiscal.certificate']
         hj = datetime.now()
         dia_vencimento = hj + relativedelta(days=dia_vcto)
         base_domain = [('date_expiration', '<', dia_vencimento)]
         cert_ids = cert_obj.search(base_domain)
-
+        usuarios = self.env['res.users'].search([])
         for cert in cert_ids:
             dif_venc = dia_vencimento - cert.date_expiration
-            msg = f"Certificado vence em {str(dia_vcto - dif_venc.days)} dias"
-            if dif_venc.days == 1:
-                self.env.user.notify_info(message=msg)
-            if dif_venc.days == 2:
-                self.env.user.notify_info(message=msg)
-            if dif_venc.days == 3:
-                self.env.user.notify_warning(message=msg)
-            if dif_venc.days > 4:
-                self.env.user.notify_danger(message=msg)
+            if dif_venc.days > 30:
+                continue
+            msg = f"Certificado {cert.name}, vence em {str(dia_vcto - dif_venc.days)} dias"
+            import pudb;pu.db
+            for user in usuarios:
+                # x.notify_info(message=msg)
+                if dif_venc.days > (intervalo*5):
+                    user.notify_info(message=msg)
+                elif dif_venc.days > (intervalo*3):
+                    user.notify_info(message=msg)
+                elif dif_venc.days > (intervalo*2):
+                    user.notify_warning(message=msg)
+                elif dif_venc.days > intervalo:
+                    user.notify_danger(message=msg)
+                else:
+                    user.notify_danger(message=msg)
+
+

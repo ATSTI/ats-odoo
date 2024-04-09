3a1474025c9c087794b15d4d50e16a00b315eba7
Mauricio-ATS <maurs320@gmail.com>
2024-04-09 15:02:50 -0300

    crm_lead_saude

--- contract_create_lead/models/create_lead.py
+++ contract_create_lead/models/create_lead.py
@@ -20,8 +20,41 @@ class ContractContractLead(models.Model):
         base_domain = [
             ('date_end', '>', ultimo_dia), 
             ('date_end','<', primeiro_dia),
+            ('tipo', 'not in', ('saude')),
         ]
         contract = self.search(base_domain)
+        ultimo_dia = (hj + relativedelta(day=31))
+        if hj.month+3 > 12:
+            mes = hj.month+3-12
+            mes_seguinte = hj.replace(month=mes, year=hj.year+1)
+        else:
+            mes_seguinte =  hj.replace(month=hj.month+3)
+        primeiro_dia = mes_seguinte.replace(day=1)
+        base_domain_saude = [
+            ('date_end', '>', ultimo_dia), 
+            ('date_end','<', primeiro_dia),
+            ('tipo', '=', 'saude'),
+        ]
+        contract += self.search(base_domain_saude)    
+        for ctr in contract:
+            crm_lead = self.env["crm.lead"].search([
+                ('name', '=', ctr.name),
+                ('partner_id', '=', ctr.partner_id.id),
+                ('stage_id', '=', 1)
+            ])
+            if crm_lead:
+                continue 
+            vals={
+                "name": ctr.name,
+                "partner_id": ctr.partner_id.id,
+            }
+            lead = self.env["crm.lead"].create(vals)
+            self.message_post(
+                body=_("Criado a Lead <b>%s<b> " %(lead.name)),
+                subject=_("Criado uma oportunidade"),
+                message_type="notification"
+            )
+        
         for ctr in contract:
             crm_lead = self.env["crm.lead"].search([
                 ('name', '=', ctr.name),

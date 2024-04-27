efd6c4593f9d2f9a4bb6bc5d058284a896cedba7
Carlos Silveira <crsilveira@gmail.com>
2024-04-27 11:39:43 -0300

    mensagem, tentando colocar o link pra correcao.

--- l10n_br_nfe_mensagem/models/document.py
+++ l10n_br_nfe_mensagem/models/document.py
@@ -10,30 +10,35 @@ class FiscalDocument(models.Model):
 
     def _valida_xml(self, xml_file):
         self.ensure_one()
-        import wdb
-        wdb.set_trace()
         erros = Nfe.schema_validation(xml_file)
         erros = "\n".join(erros)
         lista_erros = erros.splitlines()
-        for erros_msg in lista_erros:            
+        msg = ""
+        for erros_msg in lista_erros:
+            # import pudb;pu.db
+            # data-oe-model="stock.picking" t-att-data-oe-id="picking.id">
+            # link_partner = f"<a href=# data-oe-model='{self.partner_id._name}' data-oe-id='{self.partner_id.id}'>CORRIGIR</a>"
+            # link_partner = f"<a href=#id={self.partner_id.id}&model={self.partner_id._name}>CORRIGIR</a>"
+            link_partner = ""
             max_len = erros_msg.find('maxLength')
             if max_len > 0:
                 campo_erro = erros_msg[45:max_len-11]
                 if campo_erro == "xLgr":
-                    erros += " \n Rua + Bairro + Complemento: máximo 60 caracteres."
+                    msg += f" \n Rua + Bairro + Complemento: máximo 60 caracteres. {link_partner}"
                 if campo_erro == "xNome":
-                    erros += " \n Nome: máximo 60 caracteres."
+                    msg += f" \n Nome: máximo 60 caracteres.{link_partner}"
                 if campo_erro == "xFant":
-                    erros += " \n Razão social: máximo 60 caracteres."
+                    msg += f" \n Razão social: máximo 60 caracteres.{link_partner}"
             max_len = erros_msg.find('CNPJ')
             max1_len = erros_msg.find('xNome')
             if max_len > 0 and max1_len > 0 and max_len > max1_len:
-                erros += " \n Campo Cnpj/Cpf não preenchido."
+                msg += f" \n Campo Cnpj/Cpf não preenchido.{link_partner}"
             max_len = erros_msg.find('nro')
             max1_len = erros_msg.find('xBairro')
             if max1_len < 0:
                 max1_len = erros_msg.find('xCpl')
-            if max_len > 0 and max_len > max1_len:
-                erros += " \n Campo número no Endereço não preenchido."
-
+            if max_len > 0 and max1_len > 0 and max_len > max1_len:
+                msg += " \n Campo número no Endereço não preenchido." + link_partner
+        if msg:
+            erros = msg
         self.write({"xml_error_message": erros or False})

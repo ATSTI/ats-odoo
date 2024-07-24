0821870236ef168e7f83fa5646670bd503e57be2
Mauricio-ATS <maurs320@gmail.com>
2024-07-24 15:40:53 -0300

    report_pos_ewc

--- pos_receipt_custom_ewc/__manifest__.py
+++ pos_receipt_custom_ewc/__manifest__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'Recibo POS customização',
+    'name': 'Recibo POS customização EWC',
     'author': 'Odoo S.A',
     'category': 'Accounting/Localizations/Point of Sale',
-    'description': """Customização para o recibo da Campos Flores, pos""",
+    'description': """Customização para o recibo da EWC, pos""",
     'license': 'LGPL-3',
     'depends': ['point_of_sale'],
     'data': [

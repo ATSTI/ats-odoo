92f5ef3826bcc495d2e2160dc15d15bf74fa259e
Carlos Silveira <crsilveira@gmail.com>
2024-08-22 12:06:18 -0300

    correcao autor.

--- br_odoo_12/__manifest__.py
+++ br_odoo_12/__manifest__.py
@@ -1,13 +1,13 @@
 # © 2024 Carlos Silveira <crsilveira@gmail.com>
 # License AGPL-3.0 or later (http://atsti.com.br).
 
-{  # pylint: disable=C8101,C8103
+{  
     'name': 'Mantendo dados odoo-brasil para odoo 14',
     'summary': """Permite manter histórico das NF-e emitidas pelo modulo odoo-brasil(TrustCode)""",
     'description': 'NF-e dados Trustcode',
     'version': '12.0.1.0.0',
     'category': 'account',
-    'author': 'Trustcode',
+    'author': 'ATSTi',
     'license': 'AGPL-3',
     'website': '',
     'contributors': [

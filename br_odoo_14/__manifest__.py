cb0de0b9511b8d6b040ff2fe247bce11a6661247
Carlos Silveira <crsilveira@gmail.com>
2024-08-26 13:44:02 -0300

    utilizado para importar dados odoo-brasil para l10n-brazil

---
+++ br_odoo_14/__manifest__.py
@@ -0,0 +1,23 @@
+# © 2024 Carlos Silveira <crsilveira@gmail.com>
+# License AGPL-3.0 or later (http://atsti.com.br).
+
+{  
+    'name': 'Atualizando l10n-brazil com dados odoo-brasil na v 14',
+    'summary': """Copia informações dos Produtos e Parceiros da odoo-brasil para o l10n-brazil""",
+    'description': 'NF-e dados Trustcode',
+    'version': '14.0.1.0.0',
+    'category': 'account',
+    'author': 'ATSTi',
+    'license': 'AGPL-3',
+    'website': '',
+    'contributors': [
+        'Carlos Silveira <crsiveira@gmail.com>',
+    ],
+    'depends': [
+        'base', 'account',
+    ],
+    'data': [
+    ],
+    "post_init_hook": "post_init_hook",
+    'installable': True,
+}

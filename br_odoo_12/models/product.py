e73b1d736af2c610827e1d9fcf3d665aca2e4dde
Carlos Silveira <crsilveira@gmail.com>
2024-08-26 13:42:27 -0300

    correcao cad produto

--- br_odoo_12/models/product.py
+++ br_odoo_12/models/product.py
@@ -5,9 +5,25 @@ from .cst import ORIGEM_PROD
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    fiscal_type_bkp = fields.Selection(
+    type_bkp = fields.Selection(
         [('service', u'Serviço'), ('product', 'Produto')], 'Tipo Fiscal',
         )
+    fiscal_type_bkp = fields.Selection(
+        selection=[
+            ("00", "Mercadoria para Revenda"),
+            ("01", "Matéria-prima"),
+            ("02", "Embalagem"),
+            ("03", "Produto em Processo"),
+            ("04", "Produto Acabado"),
+            ("05", "Subproduto"),
+            ("06", "Produto Intermediário"),
+            ("07", "Material de Uso e Consumo"),
+            ("08", "Ativo Imobilizado"),
+            ("09", "Serviços"),
+            ("10", "Outros insumos"),
+            ("99", "Outras"),
+        ],
+    )
     origin_bkp = fields.Selection(ORIGEM_PROD, 'Origem', default='0')
     ncm = fields.Char(string="NCM", size=14)
     code_servico = fields.Char(u'Código Serviço', size=16)

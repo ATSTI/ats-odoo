f023c232b2b179667a6eca013f81b6f60a57e82b
Carlos Silveira <crsilveira@gmail.com>
2024-08-26 13:51:58 -0300

    removendo campos q nao pertence ao modulo.

--- product_sale_margin/models/product.py
+++ product_sale_margin/models/product.py
@@ -7,18 +7,6 @@ class ProductTemplate (models.Model):
     _inherit = 'product.template'
 
     margin = fields.Integer(string="Margem (%)")
-    qtde_atacado = fields.Integer(string="Qtde. Atacado")
-    preco_atacado = fields.Float(string="Preço Atacado")
-    tipo_venda = fields.Selection([
-        ('1', '1 - Normal'),
-        ('2', '2 - Venda Extra'),
-        ('3', '3 - Melhor Beneficio'),
-        ('4', '4 - Atacado'),
-        ('5', '5 - Curso'),
-        ('6', '6 - Outros'),
-        ('7', '7 - Quinta-feira do Chocolate'),
-        ], 'Tipo de Venda', default='1')
-
 
     @api.onchange('margin')
     def onchange_margin(self):
@@ -32,4 +20,3 @@ class ProductTemplate (models.Model):
             if 'standard_price' in vals:
                 vals['margin'] = round(((vals['list_price'] / vals['standard_price']) - 1),2) * 100
         return super(ProductTemplate, self).write(vals)
- 

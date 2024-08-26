bcfa03e85abfb0677edd45f9564933cf1bd9aea6
Carlos Silveira <crsilveira@gmail.com>
2024-08-26 13:52:50 -0300

    campos solicitados

--- product_sale_margin_felicita/models/product.py
+++ product_sale_margin_felicita/models/product.py
@@ -6,7 +6,7 @@ from odoo import fields,models,api, _
 class ProductTemplate (models.Model):
     _inherit = 'product.template'
 
-    margin = fields.Integer(string="Margem (%)")
+    # margin = fields.Integer(string="Margem (%)")
     qtde_atacado = fields.Integer(string="Qtde. Atacado")
     preco_atacado = fields.Float(string="Preço Atacado")
     tipo_venda = fields.Selection([
@@ -19,17 +19,3 @@ class ProductTemplate (models.Model):
         ('7', '7 - Quinta-feira do Chocolate'),
         ], 'Tipo de Venda', default='1')
     image_promocao_512 = fields.Image("Promoção (*.jpg)", max_width=512, max_height=512, store=True)
-
-    @api.onchange('margin')
-    def onchange_margin(self):
-        if self.margin:
-            self.list_price = self.standard_price * (1 + (float(self.margin)/100))
-
-    def write(self, vals):
-        if 'list_price' in vals:
-            if self.standard_price:
-                vals['margin'] = round(((vals['list_price'] / self.standard_price) - 1),2) * 100
-            if 'standard_price' in vals:
-                vals['margin'] = round(((vals['list_price'] / vals['standard_price']) - 1),2) * 100
-        return super(ProductTemplate, self).write(vals)
- 

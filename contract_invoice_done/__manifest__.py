30417a229b7b729da0c441af9ebb1e1c31687006
Carlos R. Silveira <crsilveira@gmail.com>
2024-12-23 11:06:38 -0300

    modulo nao necessario mais

--- contract_invoice_done/__manifest__.py
+++ contract_invoice_done/__manifest__.py
@@ -1,8 +1,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Contrato - Criar Fatura / Nota e Boleto",
+    "name": "NÃO NECESSÁRIO - Contrato - Criar Fatura / Nota e Boleto",
     "summary": """
+        AO INVÊS DE FAZER TUDO , CRIADO "AGENDAMENTOS",
+          PARA EXECUTAR PROCESSOS SEPARADOS :
+          - CRIAR FATURA,
+          - CRIAR BOLETO,
+          - ENVIAR NF
         Ao criar a fatura, valida, envia nota e o boleto.""",
     "version": "14.0.1.0.0",
     "license": "AGPL-3",

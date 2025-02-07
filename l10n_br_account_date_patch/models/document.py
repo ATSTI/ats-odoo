a650758e3ee0b0628626a18c34d74364670566a5
Carlos R. Silveira <crsilveira@gmail.com>
2025-02-07 17:12:38 -0300

    permitindo o usuario colocar data entrada/saida ou nao

---
+++ l10n_br_account_date_patch/models/document.py
@@ -0,0 +1,24 @@
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import api, fields, models
+
+
+class FiscalDocument(models.Model):
+    _inherit = "l10n_br_fiscal.document"
+    
+    date_in_out_option = fields.Datetime(
+        string="Data entrada/saída(manual)", copy=False
+    )
+    # alguns clientes precisam deixar a data de saida em aberto
+    @api.depends("move_ids", "move_ids.date", "date_in_out_option")
+    def _compute_date_in_out(self):
+        if self.date_in_out_option:
+            self.date_in_out = self.date_in_out_option
+        else:
+            self.date_in_out = None
+
+    def _inverse_date_in_out(self):
+        if self.date_in_out_option:
+            self.date_in_out = self.date_in_out_option
+        else:
+            self.date_in_out = None

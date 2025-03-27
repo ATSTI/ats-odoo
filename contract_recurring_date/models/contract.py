17e83f7023633d1b6081c8f817bd735b7612df9f
Mauricio-ATS <maurs320@gmail.com>
2025-03-27 10:37:38 -0300

    h2_tinha_isso

--- contract_recurring_date/models/contract.py
+++ contract_recurring_date/models/contract.py
@@ -28,3 +28,39 @@ class ContractRecurrencyMixin(models.AbstractModel):
                 - relativedelta(days=1)
             )
         return next_period_date_end
+
+"""
+class ContractContract(models.Model):
+    _inherit = "contract.contract"
+
+    @api.depends(
+        "contract_line_ids.recurring_next_date",
+        "contract_line_ids.is_canceled",
+    )
+    def _compute_recurring_next_date(self):
+        # Compute the recurring_next_date on the contract based on the one
+        # defined on line level.
+        #for contract in self:
+            #recurring_next_date = contract.contract_line_ids.filtered(
+            #    lambda l: (
+            #        l.recurring_next_date
+            #        and not l.is_canceled
+            #        and (not l.display_type or l.is_recurring_note)
+            #    )
+            #).mapped("recurring_next_date")
+            # Take the earliest or set it as False if contract is stopped
+            # (no recurring_next_date).
+            #contract.recurring_next_date = (
+            #    min(recurring_next_date) if recurring_next_date else False
+            #)
+        for rec in self:
+            recurring_next_date = self.get_next_period_date_end(
+                rec.next_period_date_start,
+                rec.recurring_invoicing_type,
+                rec.recurring_invoicing_offset,
+                rec.recurring_rule_type,
+                rec.recurring_interval,
+                max_date_end=rec.date_end,
+            )
+            rec.recurring_next_date = recurring_next_date
+"""

c20edac019a9428ab04e8f41cbb2058754794e9a
Mauricio-ATS <maurs320@gmail.com>
2025-03-31 16:17:52 -0300

    related_cfop_with_duplicata

--- l10n_br_account_document_change/models/account_move.py
+++ l10n_br_account_document_change/models/account_move.py
@@ -8,4 +8,26 @@ class AccountMove(models.Model):
 
     _inherit = "account.move"
 
-    fatura_duplicata = fields.Boolean('Duplicata da Fatura?', default=True)
+    fatura_duplicata = fields.Boolean('Duplicata da Fatura?')
+
+
+    @api.onchange("invoice_line_ids")
+    def onchange_invoice_line_ids(self):
+        import pudb;pu.db
+        for line in self.invoice_line_ids:
+            if line.cfop_id.finance_move == True:
+                self.write({"fatura_duplicata": True})
+            else:
+                self.fatura_duplicata = False
+
+# class AccountMoveLine(models.Model):
+
+#     _inherit = "account.move.line"
+
+#     @api.onchange("cfop_id")
+#     def fatura_dupli(self):
+#         import pudb;pu.db
+#         if self.cfop_id.finance_move == True:
+#             self.move_id.write({"fatura_duplicata": True})
+#         else:
+#             self.move_id.fatura_duplicata = False

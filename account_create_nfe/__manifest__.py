1721d3f5a091d2bb59d074d641a6cd284a5215ae
Carlos R. Silveira <crsilveira@gmail.com>
2025-04-28 18:35:21 -0300

    adicionado na descricao a Rotina usada no cron

--- account_create_nfe/__manifest__.py
+++ account_create_nfe/__manifest__.py
@@ -11,7 +11,29 @@
         Cria nova account.move somente com produtos, sem serviços
         No diario diversos(general), somente para poder gerar a NFe,
         pois, na fatura consta serviços tbém, que será gerado uma nota de serviço.
-   """,
+
+        # CRON, usado para chamar a funcao copiar_fatura:
+        hoje = datetime.datetime.today()
+        date_min = "-".join((str(hoje.year), str(hoje.month), "01"))
+        date_min = datetime.datetime.strptime(date_min, "%Y-%m-%d")
+        date_max = (hoje + dateutil.relativedelta.relativedelta(months=1)).replace(day=1) - datetime.timedelta(days=1)
+        msg = ''
+        notas = model.search([
+        ('create_date', '>=', date_min),
+        ('create_date', '<=', date_max),
+        ('state', '=', 'posted'),
+        ('ref', 'not in', ['NFe', 'Serviço']),
+        ('journal_id', '=', 1)
+        ])
+        #log(str(notas), level='info')
+        for nf in notas:
+        if nf.payment_mode_id:
+            msg = 'NFe a fazer : %s' %(nf.name)
+            nf.copiar_fatura()
+            msg += ' , Fatura criada com sucesso'
+        if msg:
+            log(msg, level='info')        
+    """,
     'author': 'ATS Soluções',
     'website': '',
     'depends': ["l10n_br_fiscal", "l10n_br_account_nfe"],

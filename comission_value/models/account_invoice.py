d5c066246ffd817d2ac62e26e70de509c1614bb6
crsilveira <crsilveira@gmail.com>
2025-06-13 15:31:16 -0300

    Arrumando rateio valores

--- comission_value/models/account_invoice.py
+++ comission_value/models/account_invoice.py
@@ -11,15 +11,23 @@ class AccountMove(models.Model):
                                 readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]})
 
     @api.onchange('commission_value')
-    def _onchange_comission_value(self):
+    def _onchange_comission_value(self):        
         for move in self:
             if move.commission_value:
                 value = move.commission_value - move.amount_untaxed
                 value_percent = (value * 100 / move.amount_untaxed) * -1
+                value_comission = move.commission_value / len(move.invoice_line_ids)
+                comission_total = move.commission_value
+                total_itens = 0
                 for line in move.with_context(check_move_validity=False).line_ids:
                     # credito Venda de produtos
                     if line.account_id.user_type_id.internal_group == "income" and line.credit:
-                        line.credit = move.commission_value
+                        total_itens += 1
+                        if total_itens == len(move.invoice_line_ids):
+                            line.credit = round(comission_total, 2)                            
+                        else:
+                            line.credit = round(value_comission, 2)
+                        comission_total -= round(value_comission, 2)
                     # Ipi credito
                     if line.account_id.user_type_id.internal_group == "liability" and line.credit:
                         line.credit = value * (-1)
@@ -31,6 +39,7 @@ class AccountMove(models.Model):
                     # clientes a receber
                     if line.account_id.user_type_id.internal_group == "asset" and line.debit:
                         line.debit = move.commission_value
+                        
 
                 move.update({
                     'amount_tax': value,

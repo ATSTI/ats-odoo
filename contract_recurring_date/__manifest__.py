84f4e57107de148077926d19c809474c09f7a607
Mauricio-ATS <maurs320@gmail.com>
2025-08-14 16:12:13 -0300

    Marcadores_da_fatura_na_venda

--- contract_recurring_date/__manifest__.py
+++ contract_recurring_date/__manifest__.py
@@ -8,10 +8,11 @@
     "license": "AGPL-3",
     "author": "",
     "website": "https://github.com/OCA/contract",
-    "depends": ["l10n_br_account", "contract"],
+    "depends": ["l10n_br_account", "contract", "l10n_br_sale"],
     "development_status": "Production/Stable",
     "data": [
         "views/account.xml",
+        "views/sale_view.xml",
     ],
     # "installable": True,
 }

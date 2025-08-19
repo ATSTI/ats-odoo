00e293040112a45cbf8901f69d7b61ca7d5d95e8
Mauricio-ATS <maurs320@gmail.com>
2025-08-19 09:50:46 -0300

    Relacioandno_contrato_com_venda_tags

--- contract_recurring_date/models/sale.py
+++ contract_recurring_date/models/sale.py
@@ -5,4 +5,9 @@ from odoo import api, fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    sale_tag_ids = fields.Many2many(comodel_name="contract.tag", string="Marcadores (FATURA)")
+    sale_tag_ids = fields.Many2many(comodel_name="contract.tag", compute="_compute_sale_tag_ids", string="Marcadores (FATURA)")
+
+    @api.depends("sale_tag_ids")
+    def _compute_sale_tag_ids(self):
+        for order in self:
+            order.sale_tag_ids = order.partner_id.contract_ids.tag_ids

e0ab2e286b2b921e9806012f7515617379224e7a
crsilveira <crsilveira@gmail.com>
2025-08-27 14:06:22 -0300

    Quando existe posicao fiscal no cliente, busca se existe uma operação com esta posicao fiscal

--- l10n_br_account_operation/models/account_move.py
+++ l10n_br_account_operation/models/account_move.py
@@ -13,3 +13,14 @@ class AccountMove(models.Model):
             for line in self.invoice_line_ids:
                 line.fiscal_operation_id = self.fiscal_operation_id.id
                 line._onchange_fiscal_operation_id()
+
+    @api.onchange("partner_id")
+    def _onchange_partner_id_fiscal(self):
+        res = super()._onchange_partner_id_fiscal()
+        if self.partner_id and self.partner_id.property_account_position_id:
+            operation = self.env['l10n_br_fiscal.operation'].search([
+                ('fiscal_position_id', '=', self.partner_id.property_account_position_id.id)
+            ], limit=1)
+            if operation:
+                self.fiscal_operation_id = operation.id
+        return res

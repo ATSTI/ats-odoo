5ac06edad7c83ee4a98e25471c2b286c0d0a7bbd
crsilveira <crsilveira@gmail.com>
2025-09-10 12:58:24 -0300

    Buscando linha da operaco pelo ncm

---
+++ l10n_br_fiscal_operation_by_ncm/__manifest__.py
@@ -0,0 +1,20 @@
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Operation Line by NCM/Tax Definition",
+    "category": "Localisation",
+    "summary": "Se em linhas da operação informada NCM, usar este criterio na seleção da linha",
+    "license": "AGPL-3",
+    "author": "ATSTi,Odoo Community Association (OCA)",
+    "version": "16.0.0.0.1",
+    "maintainers": ["Carlos , Mauricio"],
+    "depends": [
+        "l10n_br_fiscal",
+    ],
+    "data": [
+    ],
+    "demo": [
+    ],
+    "installable": True,
+    "auto_install": False,
+}

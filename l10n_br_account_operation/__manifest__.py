fd78ebb593c163f2e05c9f6ecd00c45ff03cdaa8
crsilveira <crsilveira@gmail.com>
2025-09-10 12:57:33 -0300

    Correção manifest

--- l10n_br_account_operation/__manifest__.py
+++ l10n_br_account_operation/__manifest__.py
@@ -1,16 +1,15 @@
-# Copyright (C) 2009 - TODAY Renato Lima - Akretion
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    "name": "Alterar operação da linha quando alterado operação principal",
+    "name": "Altera operação da linha quando alterado operação principal",
     "category": "Localisation",
     "license": "AGPL-3",
-    "author": "Akretion, Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/l10n-brazil",
-    "version": "14.0.10.1.1",
+    "author": "ATSTi,Odoo Community Association (OCA)",
+    "version": "16.0.0.0.1",
     "maintainers": ["Carlos , Mauricio"],
     "depends": [
         "l10n_br_account",
+        "l10n_br_fiscal",
     ],
     "data": [
     ],

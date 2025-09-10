6718c2dd89d5a471b756dc4aba25b33bebc19e73
Mauricio-ATS <maurs320@gmail.com>
2025-09-10 17:32:59 -0300

    View_Security_IBSCBS

---
+++ l10n_br_nfe_nt25/__manifest__.py
@@ -0,0 +1,25 @@
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "NF-e Mensagens",
+    "summary": """
+        Adiciona mensagem de validação campos obrigatorios do cliente
+        na fatura, e deixa algumas msg de validação da nfe mais fáceis,
+        e exibe na fatura as mensagens de validaçao.
+    """,
+    "category": "Localisation",
+    "license": "AGPL-3",
+    "author": "ATSTi, ",
+    "website": "",
+    "version": "14.0.1.0.0",
+    "depends": ["l10n_br_account", "l10n_br_nfe"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/ibscbs_views.xml",
+    ],
+    "demo": [
+    ],
+    "installable": True,
+    "auto_install": False,
+    "maintainers": ["carlos"],
+}

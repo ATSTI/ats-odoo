41ad31d95704561448e2d8a3d21fc7ac093f9818
Mauricio-ATS <maurs320@gmail.com>
2025-09-10 14:29:41 -0300

    Report_MM_ajustes_(falta logo das redes sociais)

--- report_mm/models/res.partner.py
+++
@@ -1,9 +0,0 @@
-from odoo import models,fields
-
-
-
-class Partner(models.Model):
-    _inherit = 'res.partner'
-
-    contact_ids = fields.One2many('res.partner', 'parent_id', string='Contatos')
-

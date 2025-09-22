02adf96c8547424e515c85c2cecabda755aa091a
crsilveira <crsilveira@gmail.com>
2025-09-22 16:47:48 -0300

    upgrade  odoo 16

--- br_odoo_12/models/account_fiscal_position.py
+++ br_odoo_12/models/account_fiscal_position.py
@@ -7,54 +7,54 @@ class AccountFiscalPositionTemplate(models.Model):
     _inherit = 'account.fiscal.position.template'
 
     ind_final = fields.Selection([
-        ('0', u'Não'),
-        ('1', u'Consumidor final')
-    ], u'Operação com Consumidor final',
-        help=u'Indica operação com Consumidor final.')
+        ('0', 'Não'),
+        ('1', 'Consumidor final')
+    ], 'Operação com Consumidor final',
+        help='Indica operação com Consumidor final.')
     ind_pres = fields.Selection([
-        ('0', u'Não se aplica'),
-        ('1', u'Operação presencial'),
-        ('2', u'Operação não presencial, pela Internet'),
-        ('3', u'Operação não presencial, Teleatendimento'),
-        ('4', u'NFC-e em operação com entrega em domicílio'),
-        ('5', u'Operação presencial, fora do estabelecimento'),
-        ('9', u'Operação não presencial, outros'),
-    ], u'Tipo de operação',
-        help=u'Indicador de presença do comprador no\n'
-             u'estabelecimento comercial no momento\n'
-             u'da operação.', default='0')
+        ('0', 'Não se aplica'),
+        ('1', 'Operação presencial'),
+        ('2', 'Operação não presencial, pela Internet'),
+        ('3', 'Operação não presencial, Teleatendimento'),
+        ('4', 'NFC-e em operação com entrega em domicílio'),
+        ('5', 'Operação presencial, fora do estabelecimento'),
+        ('9', 'Operação não presencial, outros'),
+    ], 'Tipo de operação',
+        help='Indicador de presença do comprador no\n'
+             'estabelecimento comercial no momento\n'
+             'da operação.', default='0')
 
 
 class AccountFiscalPosition(models.Model):
     _inherit = 'account.fiscal.position'
 
     finalidade_emissao = fields.Selection(
-        [('1', u'Normal'),
-         ('2', u'Complementar'),
-         ('3', u'Ajuste'),
-         ('4', u'Devolução')],
-        u'Finalidade', help=u"Finalidade da emissão de NFe", default="1")
+        [('1', 'Normal'),
+         ('2', 'Complementar'),
+         ('3', 'Ajuste'),
+         ('4', 'Devolução')],
+        'Finalidade', help=u"Finalidade da emissão de NFe", default="1")
     ind_final = fields.Selection([
-        ('0', u'Não'),
-        ('1', u'Sim')
-    ], u'Consumidor final?',
-        help=u'Indica operação com Consumidor final. Se não utilizado usa\
+        ('0', 'Não'),
+        ('1', 'Sim')
+    ], 'Consumidor final?',
+        help='Indica operação com Consumidor final. Se não utilizado usa\
         a seguinte regra:\n 0 - Normal quando pessoa jurídica\n1 - Consumidor \
         Final quando for pessoa física')
     ind_pres = fields.Selection([
-        ('0', u'Não se aplica'),
-        ('1', u'Operação presencial'),
-        ('2', u'Operação não presencial, pela Internet'),
-        ('3', u'Operação não presencial, Teleatendimento'),
-        ('4', u'NFC-e em operação com entrega em domicílio'),
-        ('5', u'Operação presencial, fora do estabelecimento'),
-        ('9', u'Operação não presencial, outros'),
-    ], u'Tipo de operação',
-        help=u'Indicador de presença do comprador no\n'
-             u'estabelecimento comercial no momento\n'
-             u'da operação.', default='0')
+        ('0', 'Não se aplica'),
+        ('1', 'Operação presencial'),
+        ('2', 'Operação não presencial, pela Internet'),
+        ('3', 'Operação não presencial, Teleatendimento'),
+        ('4', 'NFC-e em operação com entrega em domicílio'),
+        ('5', 'Operação presencial, fora do estabelecimento'),
+        ('9', 'Operação não presencial, outros'),
+    ], 'Tipo de operação',
+        help='Indicador de presença do comprador no\n'
+             'estabelecimento comercial no momento\n'
+             'da operação.', default='0')
     # TODO Fazer este campo gerar mensagens dinamicas
-    note = fields.Text(u'Observações')
+    note = fields.Text('Observações')
 
 
 # class AccountFiscalPositionTax(models.Model):

af6991253f2911c820e7dcf9816026d2d39c5e40
crsilveira <crsilveira@gmail.com>
2025-09-22 12:49:28 -0300

    Limpeza e compatibilidade odoo 16

--- br_odoo_12/__manifest__.py
+++ br_odoo_12/__manifest__.py
@@ -2,10 +2,10 @@
 # License AGPL-3.0 or later (http://atsti.com.br).
 
 {  
-    'name': 'Mantendo dados odoo-brasil para odoo 14',
-    'summary': """Permite manter histórico das NF-e emitidas pelo modulo odoo-brasil(TrustCode)""",
-    'description': 'NF-e dados Trustcode',
-    'version': '12.0.1.0.0',
+    'name': 'Mantendo dados odoo-brasil para l10n-brazil',
+    'summary': """Permite manter histórico das NF-e emitidas pelo modulo odoo-brasil""",
+    'description': 'NF-e - odoo-brasil',
+    'version': '16.0.1.0.0',
     'category': 'account',
     'author': 'ATSTi',
     'license': 'AGPL-3',
@@ -21,8 +21,6 @@
         'views/account_fiscal_position.xml',
         'views/br_odoo_nfe_view.xml',
         'views/br_odoo_nfe_item_view.xml',
-        # 'views/br_nfe.xml',
-        # 'views/br_account_view.xml',
         'views/partner_view.xml',
         'views/product_view.xml',
     ],
--- br_odoo_12/models/res_state_city.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models, _
-
-
-class ResStateCity(models.Model):
-    _inherit = 'res.state.city'
-    
-    ibge_code = fields.Char(string=u'IBGE Code', size=7, copy=False)

9fd51b07c5fd5f39a68b3254a8e5ddd22f87dae1
Mauricio-ATS <maurs320@gmail.com>
2025-10-07 15:52:51 -0300

    IBSCBS - Total / Imposto

--- l10n_br_account_nfe_IBSCBS/models/account_move.py
+++ l10n_br_account_nfe_IBSCBS/models/account_move.py
@@ -4,14 +4,14 @@ from odoo.exceptions import UserError
 
 IBSCBSALIQ_CST = [
     (
-        "01",
+        "000",
         (
             "Operação Tributável - Base de Cálculo = Valor da Operação Alíquota "
             "Normal (Cumulativo/Não Cumulativo)"
         ),
     ),
     (
-        "02",
+        "002",
         (
             "Operação Tributável - Base de Calculo = Valor da Operação (Alíquota "
             "Diferenciada)"
@@ -25,4 +25,4 @@ class AccountMove(models.Model):
     
     ibscbs = fields.Boolean('IBSCBS')
 
-    cst_ibscbs = fields.Selection(IBSCBSALIQ_CST, string='Garantia do Comprador', default='01')
+    cst_ibscbs = fields.Selection(IBSCBSALIQ_CST, string='Garantia do Comprador', default='000')
--- l10n_br_account_nfe_IBSCBS/models/document.py
+++ l10n_br_account_nfe_IBSCBS/models/document.py
@@ -4,62 +4,70 @@ from odoo import models
 from erpbrasil.base.misc import punctuation_rm
 
 
-class FiscalDocumentIBSCBS(models.Model):
-    _inherit = "l10n_br_fiscal.document"
+# class FiscalDocumentIBSCBS(models.Model):
+#     _inherit = "l10n_br_fiscal.document"
 
-    def _export_fields(self, xsd_fields, class_obj, export_dict):
-        if class_obj._name == "nfe.40.total":
-            if "nfe40_IBSCBSTot" in class_obj._fields:
-                if self.move_ids.ibscbs:
-                    # ibscbs_vals = {
-                    #     "nfe40_vBCIBSCBS": 2260.00,
-                    #     "nfe40_gIBS": {
-                    #         "nfe40_gIBSUF": {
-                    #             "nfe40_vDif": 0.00,
-                    #             "nfe40_vDevTrib": 0.00,
-                    #             "nfe40_vIBSUF": 2.26,
-                    #         },
-                    #         "nfe40_gIBSMun": {
-                    #             "nfe40_vDif": 0.00,
-                    #             "nfe40_vDevTrib": 0.00,
-                    #             "nfe40_vIBSMun": 0.00,
-                    #         },
-                    #         "nfe40_vIBS": 2.26,
-                    #         "nfe40_vCredPres": 0.00,
-                    #         "nfe40_vCredPres": 0.00,
-                    #     },
-                    #     "nfe40_gCBS": {
-                    #         "nfe40_vDif": 0.00,
-                    #         "nfe40_vDevTrib": 0.00,
-                    #         "nfe40_vCBS": 20.34,
-                    #         "nfe40_vCredPres": 0.00,
-                    #         "nfe40_vCredPresCondSus": 0.00,
-                    #     },
-                    # }
-                    # class_obj.nfe40_IBSCBSTot = self.env['nfe.40.ibscbstot'].create({})
-                    self.nfe40_vBCIBSCBS = 2260.00
-                    self.nfe40_vIBS = 2.26
-                    self.nfe40_vIBSUF = 1.00
-                    self.nfe40_vDif = 1.00 
-                    self.nfe40_vCredPres = 1.00
-                    self.nfe40_vDevTrib = 1.00
-                    self.nfe40_vIBSMun = 1.00
-                    self.nfe40_vCBS = 20.34
-                    self.nfe40_vCredPresCondSus = 1.00
-                else:
-                    export_dict["nfe40_IBSCBSTot"] = None
-        if class_obj._name == "nfe.40.infnfe":
-            if "nfe40_det" in class_obj._fields and self.nfe40_det:
-                import pudb;pu.db
-                # if self.move_ids.cst_ibscbs:
-                #     self.nfe40_det.nfe40_CST = self.move_ids.cst_ibscbs
+#     def _export_fields(self, xsd_fields, class_obj, export_dict):
+#         if class_obj._name == "nfe.40.total":
+#             if "nfe40_IBSCBSTot" in class_obj._fields:
+#                     print('oi')
+#                 else:
+#                     export_dict["nfe40_IBSCBSTot"] = None
 
-        return super()._export_fields(xsd_fields, class_obj, export_dict)
+#         return super()._export_fields(xsd_fields, class_obj, export_dict)
     
 
-# class DocumentLine(models.Model):
-#     _inherit = "l10n_br_fiscal.document.line"
+class DocumentLine(models.Model):
+    _inherit = "l10n_br_fiscal.document.line"
 
-#     def _export_fields_ibscbs(self, xsd_fields, class_obj, export_dict):
-#         import pudb;pu.db
-#         export_dict["CST"] = self.move_id.cst_ibscbs
+    def _export_fields_nfe_40_ibscbs(self, xsd_fields, class_obj, export_dict):
+        if self.document_id.move_ids.ibscbs:
+            export_dict["CST"] = self.document_id.move_ids.cst_ibscbs
+            export_dict["cClassTrib"] = "000002"
+
+    def _export_fields_nfe_40_gibscbs(self, xsd_fields, class_obj, export_dict):
+        if self.document_id.move_ids.ibscbs:
+            export_dict["vBC"] = 1.00        
+
+    def _export_fields_nfe_40_gibsuf(self, xsd_fields, class_obj, export_dict):
+        if self.document_id.move_ids.ibscbs:
+            export_dict["pIBSUF"] = 1.00
+            export_dict["vIBSUF"] = 1.00
+
+    def _export_fields_nfe_40_gibsmun(self, xsd_fields, class_obj, export_dict):
+        if self.document_id.move_ids.ibscbs:
+            export_dict["pIBSMun"] = 1.00
+            export_dict["vIBSMun"] = 1.00
+
+    def _export_fields_nfe_40_gcbs(self, xsd_fields, class_obj, export_dict):
+        if self.document_id.move_ids.ibscbs:
+            export_dict["pCBS"] = 1.00
+            export_dict["vCBS"] = 1.00
+
+    # IBSCBS Total
+    def _export_fields_nfe_40_ibscbstot(self, xsd_fields, class_obj, export_dict):
+        if self.document_id.move_ids.ibscbs:
+            export_dict["vBCIBSCBS"] = 2260.0
+
+    def _export_fields_nfe_40_gibs(self, xsd_fields, class_obj, export_dict):
+        if self.document_id.move_ids.ibscbs:
+            export_dict["vIBS"] = 2.26
+            export_dict["vCredPres"] = 0.0
+
+    def _export_fields_nfe_40_tgibsuf(self, xsd_fields, class_obj, export_dict):
+        if self.document_id.move_ids.ibscbs:
+            export_dict["vDif"] = 1.00
+            export_dict["vIBSUF"] = 1.00
+            export_dict["vDevTrib"] = 0.00
+
+    def _export_fields_nfe_40_tgibsmun(self, xsd_fields, class_obj, export_dict):
+        if self.document_id.move_ids.ibscbs:
+            export_dict["vDif"] = 1.00
+            export_dict["vIBSMun"] = 1.00
+            export_dict["vDevTrib"] = 0.00
+    
+    def _export_fields_nfe_40_tgcbs(self, xsd_fields, class_obj, export_dict):
+        if self.document_id.move_ids.ibscbs:
+            export_dict["vCBS"] = 1.00
+            export_dict["vCredPres"] = 0.0
+            export_dict["vCredPresCondSus"] = 0.0

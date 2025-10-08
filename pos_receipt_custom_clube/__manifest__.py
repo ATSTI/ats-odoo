2a1c95ccf0fcc18656327f21ab9b89ac3e192cb5
OtavioAndretta <otavio12257@gmail.com>
2025-10-08 11:04:59 -0300

    alteração_pdv_clube_impressao

--- pos_receipt_custom_clube/__manifest__.py
+++ pos_receipt_custom_clube/__manifest__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'name': 'Recibo POS customização Clube',
     'author': 'Odoo S.A',
     'category': 'Accounting/Localizations/Point of Sale',
     'description': """Customização para o recibo da Clube sao pedro, pos""",
     'license': 'LGPL-3',
+    'author':'ATSTi Soluções',
     'depends': ['point_of_sale'],
     'data': [
-        #'views/assets.xml',
+        # 'views/assets.xml',
     ],
     'qweb': [
         'static/src/xml/OrderReceipt.xml',

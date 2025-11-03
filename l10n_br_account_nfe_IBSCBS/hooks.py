726cf305bf3173b2715145739357edf41702d8cd
crsilveira <crsilveira@gmail.com>
2025-11-03 18:56:03 -0300

    lendo csv para cadastrar os itens

--- l10n_br_account_nfe_IBSCBS/hooks.py
+++ l10n_br_account_nfe_IBSCBS/hooks.py
@@ -1,96 +1,21 @@
 import logging
+import csv
+
+import os
+
 from odoo import SUPERUSER_ID, _, api, tools
 
 _logger = logging.getLogger(__name__)
 
 def post_init_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
-    # tax_ibscbs = env['l10n_br_fiscal.tax']
-
-    # files = ["data/account_tax_group.xml"]
-    # for file in files:
-    #     tools.convert_file(
-    #         cr,
-    #         "l10n_br_account_nfe_IBSCBS",
-    #         file,
-    #         None,
-    #         mode="init",
-    #         noupdate=True,
-    #         kind="init",
-    #     )
-
-
+    module_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(module_dir, 'data', 'l10n_br_fiscal.tax.csv')
     novos_itens = []
-    
-    # "id","name","tax_base_type",,,"tax_group_id:id",,,,,"uot_id:id","percent_debit_credit","icms_base_type","icmsst_base_type","icmsst_mva_percent","icmsst_value"
-    values = {
-        "id": "tax_ibscbs_000001",
-        "name": "IBS/CBS Integral",
-        "tax_base_type": "percent",
-        "percent_amount": 1.00,
-        "percent_reduction": 0.00,
-        "tax_group_id": "tax_group_ibscbs",
-        "cst_in_id": "cst_ibscbs_000001",
-        "cst_out_id": "cst_ibscbs_000001",
-        "value_amount": 0.0,
-        "percent_debit_credit": 0.0,
-        "icms_base_type": "0",
-        "icmsst_mva_percent": 0.0,
-        "icmsst_value": 0.0,
-        "tax_domain": "ibscbs",
-    }
-    novos_itens.append(values)
-    values = {
-        "id": "tax_ibsuf_000001",
-        "name": "IBSUF Integral",
-        "tax_base_type": "percent",
-        "percent_amount": 0.10,
-        "percent_reduction": 0.00,
-        "tax_group_id": "tax_group_ibscbs",
-        "cst_in_id": "cst_ibscbs_000001",
-        "cst_out_id": "cst_ibscbs_000001",
-        "value_amount": 0.0,
-        "percent_debit_credit": 0.0,
-        "icms_base_type": "0",
-        "icmsst_mva_percent": 0.0,
-        "icmsst_value": 0.0,
-        "tax_domain": "ibsuf",
-    }
-    novos_itens.append(values)
-    values = {
-        "id": "tax_ibsmun_000001",
-        "name": "IBSMun Integral",
-        "tax_base_type": "percent",
-        "percent_amount": 0.00,
-        "percent_reduction": 0.00,
-        "tax_group_id": "tax_group_ibscbs",
-        "cst_in_id": "cst_ibscbs_000001",
-        "cst_out_id": "cst_ibscbs_000001",
-        "value_amount": 0.0,
-        "percent_debit_credit": 0.0,
-        "icms_base_type": "0",
-        "icmsst_mva_percent": 0.0,
-        "icmsst_value": 0.0,
-        "tax_domain": "ibsmun",
-    }
-    novos_itens.append(values)
-    values = {
-        "id": "tax_cbs_000001",
-        "name": "CBS Integral",
-        "tax_base_type": "percent",
-        "percent_amount": 0.90,
-        "percent_reduction": 0.00,
-        "tax_group_id": "tax_group_ibscbs",
-        "cst_in_id": "cst_ibscbs_000001",
-        "cst_out_id": "cst_ibscbs_000001",
-        "value_amount": 0.0,
-        "percent_debit_credit": 0.0,
-        "icms_base_type": "0",
-        "icmsst_mva_percent": 0.0,
-        "icmsst_value": 0.0,
-        "tax_domain": "cbs",
-    }
-    novos_itens.append(values)            
+    with open(file_path, 'r', encoding='utf-8') as file: # Use appropriate encoding
+        csv_data = csv.DictReader(file) 
+        for row in csv_data:
+            novos_itens.append(row)
     ir_model_data_obj = env['ir.model.data']
     # Search for an existing entry
     for item in novos_itens:
@@ -175,4 +100,4 @@ def post_init_hook(cr, registry):
             "ibsmun_tax_id": id_mun_tax.res_id,
             "cbs_tax_id": id_cbs_tax.res_id,
         })
-    _logger.info(_("Empresas ataulizadas com sucesso..."))
+    _logger.info(_("Empresas atualizadas com sucesso..."))

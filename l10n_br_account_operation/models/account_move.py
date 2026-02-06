a329b27eec043d2d3ec19847e28bcbbb260b515d
crsilveira <crsilveira@gmail.com>
2026-02-06 13:24:06 -0300

    somente deve alterar a operação das linhas se alterar a operação

--- l10n_br_account_operation/models/account_move.py
+++ l10n_br_account_operation/models/account_move.py
@@ -13,18 +13,3 @@ class AccountMove(models.Model):
             for line in self.invoice_line_ids:
                 line.fiscal_operation_id = self.fiscal_operation_id.id
                 line._compute_all_tax()
-                # line._compute_fiscal_amounts()
-
-    @api.onchange("partner_id")
-    def _onchange_partner_id_fiscal(self):
-        partner = self.partner_id
-        if partner and partner.property_account_position_id:
-            operation = self.env['l10n_br_fiscal.operation'].search([
-                ('fiscal_position_id', '=', partner.property_account_position_id.id)
-            ], limit=1)
-            if operation:
-                self.fiscal_operation_id = operation.id
-            self.ind_final = partner.ind_final
-            for line in self._get_amount_lines():
-                # reload fiscal data, operation line, cfop, taxes, etc.
-                line._onchange_fiscal_operation_id()

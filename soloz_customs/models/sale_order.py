72379a8040152270929913c7e092b007dc878080
Mauricio-ATS <maurs320@gmail.com>
2026-03-12 10:39:26 -0300

    Visualizações , organização de campos e telas na Venda

--- soloz_customs/models/sale_order.py
+++ soloz_customs/models/sale_order.py
@@ -8,6 +8,21 @@ from odoo import api, fields, models, _
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
+    partner_city = fields.Char(
+        string="Cidade",
+        related="partner_id.city",
+        store=True,
+        readonly=True,
+    )
+
+    partner_state_id = fields.Many2one(
+        "res.country.state",
+        string="Estado",
+        related="partner_id.state_id",
+        store=True,
+        readonly=True,
+    )
+
     contact_partner = fields.Many2many(
         comodel_name='res.partner',
         compute='_compute_contact_partner',

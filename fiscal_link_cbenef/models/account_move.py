22059f40901eaea03f4405af76e4bf0be37e42bc
Mauricio-ATS <maurs320@gmail.com>
2026-03-17 15:41:46 -0300

    Evitando Onchange nas linhas

--- fiscal_link_cbenef/models/account_move.py
+++ fiscal_link_cbenef/models/account_move.py
@@ -2,6 +2,7 @@
 
 import re
 from odoo import models, _, api, fields
+from odoo.exceptions import UserError
 
 from odoo.addons.l10n_br_fiscal.constants.fiscal import (
     MODELO_FISCAL_NFE,
@@ -10,13 +11,15 @@ from odoo.addons.l10n_br_fiscal.constants.fiscal import (
 
 class AccountMove(models.Model):
     _inherit = "account.move"
-  
-    @api.onchange("invoice_line_ids")
-    def _onchange_invoice_line_ids(self):
-        if self.invoice_line_ids and self.document_type_id and self.document_type_id.code == MODELO_FISCAL_NFE:
-            erros = self.xml_error_message or ""
-            if self.company_id.state_id == self.env['res.country.state'].search([('code', '=', 'SP')], limit=1):
-                for line in self.invoice_line_ids:
-                    if not line.cbenef_id:
-                        erros += f"\n Erro: Sem beneficio Fiscal (cBenef) ABA ICMS , no item {line.name}"
-                self.xml_error_message = erros or False
+
+    def action_post(self):
+        for record in self:
+            if record.invoice_line_ids and record.document_type_id and record.document_type_id.code == MODELO_FISCAL_NFE:
+                erros = ""
+                if record.company_id.state_id.id in record.env['res.country.state'].search([('code', '=', 'SP')]).ids:
+                    for line in record.invoice_line_ids:
+                        if not line.cbenef_id:
+                            erros += f"\n Erro: Sem beneficio Fiscal (cBenef) ABA ICMS , no item {line.name}"
+                if erros:
+                    raise UserError(_(f"{erros}"))
+        return super().action_post()

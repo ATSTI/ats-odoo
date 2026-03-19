86df1a1a221c3806c9a48f52a9e9f7982693d674
OtavioAndretta <otavio12257@gmail.com>
2026-03-19 08:52:47 -0300

    Modulo completo customização para Soloz industrial - abrangendo nova lógica em stock.picking - relatórios filtrando campos para usuários/estágios do picking - validações internas e campo novo no crm.claim

---
+++ picking_soloz_custom/__manifest__.py
@@ -0,0 +1,37 @@
+# Copyright (C) 2025 - ATSTi
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.ht
+
+{
+    'name': 'Customizações Soloz',
+    'version': '16.0',
+    'category': 'Others',
+    'license': 'AGPL-3',
+    'sequence': 2,
+    'summary': 'Módulo de Customizações Específicas para a Soloz em stock.picking',
+    'description': """
+        Este módulo tem como finalidade algumas mecanicas que a Empresa pediu, entre elas estão:
+        - Lógica nova em stock.picking
+        - Relátorios com campos ocultos para determinados usuários/estágios do picking
+        - Estados separação/conferencia
+        - Conferencia as cegas, ocultando tabelas de DEMANDA e RESERVADO
+    """,
+    'author': 'ATSTi Soluções',
+    'maintainer': 'Otávio Andretta <otavio12257@gmail.com>',
+    'website': 'https://github.com/ATSTI/ats-odoo',
+    'depends': ['base', 'sale_management','stock','mrp','crm','stock'],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/security.xml',
+        'views/crm_claim_form_view.xml',
+        'views/stock_picking_view.xml',
+        'views/wizard_validar_conferencia.xml',
+        'report/report_operacao_separacao_total_custom.xml',
+        'report/report_picking_inherit.xml',
+        'report/report_delivery_document_inherit.xml',
+        'report/report_separacao_cega_gerente.xml',
+    ],
+
+
+    'installable': True,
+    'application': False,
+}

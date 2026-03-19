536a42ebdab45a41dc6e025be910367047d16bae
Mauricio-ATS <maurs320@gmail.com>
2026-03-19 10:35:16 -0300

    Tratamento de Certificado Error dentro do modulo de mensagem

--- l10n_br_nfe_mensagem/__manifest__.py
+++ l10n_br_nfe_mensagem/__manifest__.py
@@ -1,24 +1,26 @@
+# Copyright (C) 2025 - ATSTi
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    "name": "NF-e Mensagens",
-    "summary": """
-        Adiciona mensagem de validação campos obrigatorios do cliente
+    'name': 'NF-e Mensagens',
+    'version': '1.0',
+    'category': 'Localisation',
+    'license': 'AGPL-3',
+    'sequence': 2,
+    'summary': 'Mensagem Erro NFe',
+    'description': """
+        Adiciona mensagem de validação campos obrigatórios do cliente
         na fatura, e deixa algumas msg de validação da nfe mais fáceis,
         e exibe na fatura as mensagens de validaçao.
+        Além disso, esse módulo evita o erro de certificado expirado e exibe uma mensagem de erro mais amigável para o usuário.
     """,
-    "category": "Localisation",
-    "license": "AGPL-3",
-    "author": "ATSTi, ",
-    "website": "",
-    "version": "14.0.1.0.0",
-    "depends": ["l10n_br_account", "l10n_br_nfe"],
-    "data": [
+    'author': 'ATSTi Soluções',
+    'maintainer': 'Carlos Silveira, Mauricio-ATS, ATSTi',
+    'website': 'https://github.com/ATSTI/ats-odoo',
+    'depends': ['l10n_br_account','l10n_br_nfe', 'l10n_br_fiscal_certificate'],
+    'data': [
         "views/account_invoice_view.xml",
     ],
-    "demo": [
-    ],
-    "installable": True,
-    "auto_install": False,
-    "maintainers": ["carlos"],
+    'installable': True,
+    'application': False,
 }

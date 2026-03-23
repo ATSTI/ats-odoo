702535d82622207afdf31a3bea9d0b805e6b1cc3
OtavioAndretta <otavio12257@gmail.com>
2026-03-23 12:04:07 -0300

    Ajustes - removido códigos comentados -- ajustado imports em sale.order e stock.picking -- depends do manifest ajustado

--- picking_soloz_custom/models/stock_picking.py
+++ picking_soloz_custom/models/stock_picking.py
@@ -26,7 +26,6 @@ class Picking(models.Model):
         tracking=True,
     )
 
-    # Campo para proteger estados customizados do recompute
     state_locked = fields.Boolean(default=False, copy=False)
 
     separador_id = fields.Many2one('res.users', string='Separador', tracking=True)
@@ -43,19 +42,6 @@ class Picking(models.Model):
     def _compute_is_conferente(self):
         for rec in self:
             rec.is_conferente = rec.conferente_id.id == self.env.uid
-
-    # @api.depends()
-    # def _compute_user_domains(self):
-    #     group_sep = self.env.ref('picking_soloz_custom.group_separador', raise_if_not_found=False)
-    #     group_ger = self.env.ref('picking_soloz_custom.group_gerente_inventario', raise_if_not_found=False)
-    #     group_conf = self.env.ref('picking_soloz_custom.group_conferente', raise_if_not_found=False)
-
-    #     sep_ids = list({g.id for g in [group_sep, group_ger] if g})
-    #     conf_ids = list({g.id for g in [group_conf, group_ger] if g})
-
-    #     for rec in self:
-    #         rec.separador_domain = [('groups_id', 'in', sep_ids)] if sep_ids else []
-    #         rec.conferente_domain = [('groups_id', 'in', conf_ids)] if conf_ids else []
 
     @api.depends("move_ids_without_package.diferenca")
     def _compute_divergencias(self):

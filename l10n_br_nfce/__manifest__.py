258c3ca209ea5422d0930dd00128542b5384b914
Mauricio-ATS <maurs320@gmail.com>
2026-03-23 14:19:05 -0300

    NFC-e Adaptação , primeiro teste

---
+++ l10n_br_nfce/__manifest__.py
@@ -0,0 +1,19 @@
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "NFC-e Adaptação",
+    "summary": "Adaptação para emissão de NFC-e",
+    "category": "Localisation",
+    "license": "AGPL-3",
+    "author": "ATSTi",
+    "website": "",
+    "development_status": "Beta",
+    "version": "1.0",
+    "depends": [
+        "l10n_br_nfe",
+    ],
+    "data": [],
+    "demo": [],
+    "installable": True,
+    "auto_install": False,
+}

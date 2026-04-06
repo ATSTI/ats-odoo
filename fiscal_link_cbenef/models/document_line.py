11865264e24a2440a05285a23efbe294a04e74bb
Mauricio-ATS <maurs320@gmail.com>
2026-04-06 12:12:57 -0300

    [FIX] avoid validantions when code iis "SEM CBENEF" - EVITANDO VALIDAÇÕES NO "SEM CBENEF"

--- fiscal_link_cbenef/models/document_line.py
+++ fiscal_link_cbenef/models/document_line.py
@@ -65,9 +65,6 @@ class DocumentLineMixinMethods(models.AbstractModel):
         for rec in self:
             if not rec.cbenef_id:
                 continue
-            if rec.cbenef_id.code == 'SEM CBENEF':
-                rec.icms_tax_benefit_id = False
-                continue
             if rec.ncm_id and not rec.ncm_id.cbenef_id:
                 rec.ncm_id.cbenef_id = rec.cbenef_id.id
             Tax_Definition = rec.env['l10n_br_fiscal.tax.definition']
@@ -96,22 +93,6 @@ class DocumentLineMixinMethods(models.AbstractModel):
                 })
                 tax_benefit.display_name = f"{icms_group.name} - f{rec.icms_cst_id.code} - {rec.cbenef_id.code} - {rec.cbenef_id.description}"
                 tax_benefit._onchange_tax_id()
-            # ACREDITO q isso aqui abaixo poderia levar o usuario a fazer coisa errada, se quiser mudar tem q entrar no NCM e mudar.
-            # else:
-                # if tax_benefit.code != rec.cbenef_id.code:
-                    # rec.cbenef_id = False
-                    # erros = "Já existe um benefício fiscal cadastrado para essa CST, altere a linha da operação fiscal e o cBenef no NCM se existir."
-                    # raise UserError(_(f"{erros}"))
-                    # tax_benefit.write({
-                    #     'code': rec.cbenef_id.code,
-                    #     'description': rec.cbenef_id.description,
-                    #     'benefit_type': rec.get_benefit_type(
-                    #         rec.icms_cst_id.code if rec.icms_cst_id else False
-                    #     ),
-                    #     'state_from_id': rec.env['res.country.state'].search([('code', '=', 'SP')], limit=1).id,
-                    # })
-                    # tax_benefit.display_name = f"{icms_group.name} - f{rec.icms_cst_id.code} - {rec.cbenef_id.code} - {rec.cbenef_id.description}"
-                    # tax_benefit._onchange_tax_id()
             if rec.ncm_id and not rec.ncm_id.cbenef_id:
                 rec.ncm_id.cbenef_id = rec.cbenef_id.id
 

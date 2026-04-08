cce95223624ace22084608d760d110afdb85021b
crsilveira <crsilveira@gmail.com>
2026-04-08 15:08:04 -0300

    adicionando o cBenef para Simples nacional

--- fiscal_link_cbenef/hooks.py
+++ fiscal_link_cbenef/hooks.py
@@ -31,6 +31,18 @@ def post_init_hook(cr, registry):
                     continue
                 if key.startswith('cst') and value == 'SIM':
                     csts.append(key[3:])
+                if key.startswith('simples') and value == 'SIM':
+                    if '101' not in csts:
+                        csts.append('101')
+                        csts.append('102')
+                        csts.append('103')
+                        csts.append('201')
+                        csts.append('202')
+                        csts.append('203')
+                        csts.append('300')
+                        csts.append('400')
+                        csts.append('500')
+                        csts.append('900')
 
             cbenef_record = CBenef.search([('code', '=', row['code'])], limit=1)
             if not cbenef_record:

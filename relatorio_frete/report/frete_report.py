fc57be167ed7a3c7aa63a2e4dac6ed1d5c65e5de
crsilveira <crsilveira@gmail.com>
2026-04-14 10:49:01 -0300

    sem agrupar

--- relatorio_frete/report/frete_report.py
+++ relatorio_frete/report/frete_report.py
@@ -12,8 +12,8 @@ class FreteReport(models.AbstractModel):
     _name = "report.relatorio_frete.frete_report"
     _description = "Frete Report"
 
-    def _get_tax_data(self, tax_ids):
-        import pudb;pu.db        
+    def _get_receitas(self, document_ids):
+        # import pudb;pu.db        
         taxes = self.env["account.tax"].browse(tax_ids)
         tax_data = {}
         for tax in taxes:
@@ -32,29 +32,14 @@ class FreteReport(models.AbstractModel):
         return tax_data
 
     @api.model
-    def _get_tax_report_domain(self, company_id, date_from, date_to, only_posted_moves, accounts_ids=None):
-        import pudb;pu.db
+    def _get_frete_report_domain(self, company_id, date_from, date_to, only_posted_moves, accounts_ids=None):
+        # import pudb;pu.db
         domain = [
             ("company_id", "=", company_id),
             ("date", ">=", date_from),
             ("date", "<=", date_to),
             ("account_id", "in", accounts_ids),
-        ] + self.env["account.move.line"]._get_tax_exigible_domain()
-        if only_posted_moves:
-            domain += [("move_id.state", "=", "posted")]
-        else:
-            domain += [("move_id.state", "in", ["posted", "draft"])]
-        return domain
-
-    @api.model
-    def _get_net_report_domain(self, company_id, date_from, date_to, only_posted_moves, accounts_ids=None):
-        import pudb;pu.db        
-        domain = [
-            ("company_id", "=", company_id),
-            ("date", ">=", date_from),
-            ("date", "<=", date_to),
-            ("account_id", "in", accounts_ids),
-        ] + self.env["account.move.line"]._get_tax_exigible_domain()
+        ]
         if only_posted_moves:
             domain += [("move_id.state", "=", "posted")]
         else:
@@ -62,48 +47,57 @@ class FreteReport(models.AbstractModel):
         return domain
 
     def _get_frete_report_data(self, company_id, date_from, date_to, accounts_ids=None):
-        import pudb;pu.db
-        tax_domain = self._get_tax_report_domain(
-            company_id, date_from, date_to, True
-        )
-        ml_fields = self._get_ml_fields_frete_report()
-        tax_move_lines = self.env["account.move.line"].search_read(
-            domain=tax_domain,
-            fields=ml_fields,
-        )
-        net_domain = self._get_net_report_domain(
+        frete_domain = self._get_frete_report_domain(
             company_id, date_from, date_to, True, accounts_ids
         )
-        taxed_move_lines = self.env["account.move.line"].search_read(
-            domain=net_domain,
+        ml_fields = self._get_ml_fields_frete_report()
+
+        # linhas com frete
+        frete_move_lines = self.env["account.move.line"].search_read(
+            domain=frete_domain,
             fields=ml_fields,
         )
-        taxed_move_lines = list(filter(lambda d: d["tax_ids"], taxed_move_lines))
-        vat_data = []
-        for tax_move_line in tax_move_lines:
-            vat_data.append(
-                {
-                    "net": 0.0,
-                    "tax": tax_move_line["balance"],
-                    "tax_line_id": tax_move_line["tax_line_id"][0],
-                }
-            )
-        for taxed_move_line in taxed_move_lines:
-            for tax_id in taxed_move_line["tax_ids"]:
-                vat_data.append(
-                    {
-                        "net": taxed_move_line["balance"],
-                        "tax": 0.0,
-                        "tax_line_id": tax_id,
-                    }
-                )
-        tax_ids = list(map(operator.itemgetter("tax_line_id"), vat_data))
-        tax_ids = list(set(tax_ids))
-        tax_data = self._get_tax_data(tax_ids)
-        return vat_data, tax_data
+        related_data = []
+        for move_id in frete_move_lines:
+            related_data.append(move_id["move_id"][0])
+        related_data = list(set(related_data))
+        
+        # documentos fiscais com os documentos relacionados
+        doc_domain = [
+            ("move_ids", "in", related_data),
+        ]        
+        import pudb;pu.db
+        move_fields = ["id", "document_date", "partner_id", "document_related_ids"]
+        move_ids = self.env["l10n_br_fiscal.document"].search_read(
+            domain=doc_domain,
+            fields=move_fields,
+        )
+        # documentos relacionados
+        doc_data = []
+        for move_doc in move_ids:
+            doc_data.append(move_doc["document_related_ids"][0])
+        doc_data = list(set(doc_data))            
+
+        # lendo documentos relacionados
+        related_domain = [
+            ("id", "in", doc_data),
+        ]        
+        import pudb;pu.db
+        doc_fields = ["id", "partner_id", "document_date", "amount_freight_value"]
+        doc_ids = self.env["l10n_br_fiscal.document"].search_read(
+            domain=related_domain,
+            fields=doc_fields,
+        )
+        receita_data = []
+        for move_doc in doc_ids:
+             receita_data.append(move_doc)
+        # tax_ids = list(map(operator.itemgetter("tax_line_id"), vat_data))
+        # tax_ids = list(set(tax_ids))
+        # tax_data = self._get_receitas(document_ids)
+        return related_data, receita_data
 
     def _get_tax_group_data(self, tax_group_ids):
-        import pudb;pu.db
+        # import pudb;pu.db
         tax_groups = self.env["account.tax.group"].browse(tax_group_ids)
         tax_group_data = {}
         for tax_group in tax_groups:
@@ -119,7 +113,7 @@ class FreteReport(models.AbstractModel):
         return tax_group_data
 
     def _get_frete_report_group_data(self, vat_report_data, tax_data, tax_detail):
-        import pudb;pu.db        
+        # import pudb;pu.db        
         vat_report = {}
         for tax_move_line in vat_report_data:
             tax_id = tax_move_line["tax_line_id"]
@@ -159,7 +153,7 @@ class FreteReport(models.AbstractModel):
         return vat_report_list
 
     def _get_tags_data(self, tags_ids):
-        import pudb;pu.db
+        # import pudb;pu.db
         tags = self.env["account.account.tag"].browse(tags_ids)
         tags_data = {}
         for tag in tags:
@@ -167,7 +161,7 @@ class FreteReport(models.AbstractModel):
         return tags_data
 
     def _get_frete_report_tag_data(self, frete_report_data, tax_data, tax_detail):
-        import pudb;pu.db
+        # import pudb;pu.db
         frete_report = {}
         for tax_move_line in frete_report_data:
             tax_id = tax_move_line["tax_line_id"]
@@ -207,7 +201,7 @@ class FreteReport(models.AbstractModel):
         return frete_report_list
 
     def _get_report_values(self, docids, data):
-        import pudb;pu.db
+        # import pudb;pu.db
         res = super()._get_report_values(docids, data)
         wizard_id = data["wizard_id"]
         company = self.env["res.company"].browse(data["company_id"])

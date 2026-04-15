8d3069d604cd2245d8a9fac23ab1afc4d189e23f
crsilveira <crsilveira@gmail.com>
2026-04-15 13:14:18 -0300

    limpeza  arquivo

--- relatorio_frete/__manifest__.py
+++ relatorio_frete/__manifest__.py
@@ -1,4 +1,3 @@
-# Copyright (C) 2025 - ATSTi
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
--- relatorio_frete/wizard/frete_report_wizard.py
+++ relatorio_frete/wizard/frete_report_wizard.py
@@ -1,4 +1,3 @@
-# Copyright  2018 Forest and Biomass Romania
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import _, api, fields, models

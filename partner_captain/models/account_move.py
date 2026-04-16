113bd3e0cf122d8b746c75703a0204c2f90f044c
OtavioAndretta <otavio12257@gmail.com>
2026-04-16 12:08:01 -0300

    sem herdar metodo oficial pois ele está causando problemas

--- partner_captain/models/account_move.py
+++ partner_captain/models/account_move.py
@@ -1,13 +1,12 @@
-from odoo import models
+from odoo import models, api
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    def action_invoice_sent_with_attachments(self):
+    def action_send_email_with_attachments(self):
         self.ensure_one()
 
-        # Chama o comportamento padrão
-        action = self.action_invoice_sent()
+        template = self.env.ref('account.email_template_edi_invoice', raise_if_not_found=False)
 
         attachments = self.env['ir.attachment'].search([
             ('res_model', '=', 'account.move'),
@@ -15,22 +14,23 @@ class AccountMove(models.Model):
             ('res_field', '=', False),
         ])
 
-        ctx = dict(action.get('context', {}))
-
-        # Pega anexos existentes do template
-        existing_ids = []
-        if ctx.get('default_attachment_ids'):
-            existing = ctx['default_attachment_ids']
-            if isinstance(existing, list) and isinstance(existing[0], tuple):
-                existing_ids = existing[0][2]
-
-        all_ids = list(set(existing_ids + attachments.ids))
-
-        ctx.update({
-            'default_attachment_ids': [(6, 0, all_ids)],
+        ctx = {
+            'default_model': 'account.move',
+            'default_res_id': self.id,
+            'default_use_template': bool(template),
+            'default_template_id': template.id if template else False,
+            'default_composition_mode': 'comment',
+            'default_attachment_ids': [(6, 0, attachments.ids)],
+            'default_email_layout_xmlid': 'mail.mail_notification_paynow',
+            'force_email': True,
             'default_reply_to': 'financeiro@captaindive.com.br',
-        })
+        }
 
-        action['context'] = ctx
-
-        return action
+        return {
+            'name': 'Enviar Fatura por Email',
+            'type': 'ir.actions.act_window',
+            'res_model': 'mail.compose.message',
+            'view_mode': 'form',
+            'target': 'new',
+            'context': ctx,
+        }

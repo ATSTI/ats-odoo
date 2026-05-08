72aedfc3bb10bb78d14d208c14f89467eb45d7f7
Mauricio-ATS <maurs320@gmail.com>
2026-05-08 13:41:16 -0300

    [FIX] Consulta de pendencias; date_due and mensage

--- pending_payments/controller/main.py
+++ pending_payments/controller/main.py
@@ -7,7 +7,7 @@
 from datetime import datetime, timedelta
 import logging
 from unicodedata import normalize
-from odoo import http
+from odoo import http, fields
 from odoo.http import request
 import os
 from dotenv import load_dotenv
@@ -20,13 +20,13 @@ logger = logging.getLogger(__name__)
 class ConsultaPendencia(http.Controller):
 
     @http.route('/check-pending-payments', type='http', auth="public", csrf=False)
-    def website_check_pending_payments(self, company_name=None, comapny_vat=None, **kwargs):
+    def website_check_pending_payments(self, company_name=None, company_vat=None, **kwargs):
         if not company_name:
             return request.make_json_response({"error": "company_name é obrigatório"}, status=400)
 
         env = request.env
         cmp_name = normalize('NFKD', company_name).encode('ASCII', 'ignore').decode('ASCII')
-        partner = env['res.partner'].sudo().search(['|', ('name', 'ilike', cmp_name),('cnpj_cpf_stripped', '=', comapny_vat)], limit=1)
+        partner = env['res.partner'].sudo().search(['|', ('name', 'ilike', cmp_name),('cnpj_cpf_stripped', '=', company_vat)], limit=1)
         if not partner:
             return {"error": "Parceiro não encontrado."}
 
@@ -40,13 +40,25 @@ class ConsultaPendencia(http.Controller):
             ('state', '=', 'posted'), 
             ('payment_state', '!=', 'paid'),
             ('invoice_date', '>=', inicio),
-            ('invoice_date', '<=', fim)
+            ('invoice_date', '<=', fim),
+            ('invoice_date_due', '<=', fields.Date.today())
         ])
         pendencias_data = []
         for move in pendencias:
-            pendencias_data.append({
-                'partner_id': move.partner_id.name,
-            })
+            date_now = fields.Date.today()
+            date_due_limit = move.invoice_date_due + timedelta(days=3)
+            msg = ""
+            if date_now < date_due_limit:
+                msg = f"""Não verificamos o seu pagamento. Por favor, contate o financeiro, whatsapp: 1997104-0941\n
+                   Seu sistema será bloqueado em {(date_due_limit - date_now).days} dia(s)"""
+            if date_now >= date_due_limit:
+                msg = f"""Não verificamos o seu pagamento. Por favor, contate o financeiro, whatsapp: 1997104-0941\n
+                   SEU SISTEMA ESTÁ BLOQUEADO!!!"""
+            if msg:
+                pendencias_data.append({
+                    'pending': True,
+                    'mensagem': msg,
+                })
         return request.make_json_response({
                 "pendencias": pendencias_data
             })

889f2c9c86cdfed100076d45dfb468d52c0bffc6
crsilveira <crsilveira@gmail.com>
2026-07-28 10:59:05 -0300

    mig 16

--- l10n_br_sale_operation/models/sale_order.py
+++ l10n_br_sale_operation/models/sale_order.py
@@ -9,18 +9,18 @@ class SaleOrder(models.Model):
     # isso corrige a operacao em todas as linhas
     @api.onchange("fiscal_operation_id")
     def _onchange_fiscal_operation_id(self):
+        res = super()._onchange_fiscal_operation_id()
         if self.fiscal_operation_id:
             for line in self.order_line:
                 line.fiscal_operation_id = self.fiscal_operation_id.id
-                line._onchange_fiscal_operation_id()
+                line._compute_tax_id()
+        return res
 
     @api.onchange("partner_id")
     def _onchange_partner_id_fiscal(self):
-        res = super()._onchange_partner_id_fiscal()
         if self.partner_id and self.partner_id.property_account_position_id:
             operation = self.env['l10n_br_fiscal.operation'].search([
                 ('fiscal_position_id', '=', self.partner_id.property_account_position_id.id)
             ], limit=1)
             if operation:
                 self.fiscal_operation_id = operation.id
-        return res

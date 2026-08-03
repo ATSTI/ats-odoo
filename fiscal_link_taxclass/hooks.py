54a81e16187bdffc07ea3b5d6c5b0cb78227567e
crsilveira <crsilveira@gmail.com>
2026-08-03 20:09:53 -0300

    tax_class ncm nbs

--- fiscal_link_taxclass/hooks.py
+++ fiscal_link_taxclass/hooks.py
@@ -13,22 +13,22 @@ def post_init_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
 
     Ncm = env['l10n_br_fiscal.ncm']
-    Cfop = env['l10n_br_fiscal.cfop']
+    # Cfop = env['l10n_br_fiscal.cfop']
     Tax = env['l10n_br_fiscal.tax.classification']
 
     base_dir = os.path.dirname(__file__)
     csv_path = os.path.join(
         base_dir,
         'data',
-        'l10n_br_fiscal.ncm.tax.csv'
+        'l10n_br_fiscal.ncm.csv'
     )
-
+    
     with open(csv_path, newline='', encoding='utf-8') as f:
         reader = csv.DictReader(f)
 
         for i, row in enumerate(reader, start=1):
             ncm = row.get("code", "").strip()
-            tax = row.get("tax_classification", "").strip()
+            tax = row.get("tax_classification_id:id", "").strip()
 
             if not ncm or not tax:
                 continue
@@ -46,16 +46,16 @@ def post_init_hook(cr, registry):
                     i, ncm, code_tax
                 )
 
-    cfop_to_change = [5901, 6901, 5902, 6902, 5915, 6915]
-    cfop_records = Cfop.search([('code', 'in', cfop_to_change)])
-    for cfop in cfop_records:
-        if cfop.code in ['5901', '6901', '5915', '6915'] and not cfop.tax_classification_id:
-            cfop.tax_classification_id = Tax.search([('code', '=', '000000')], limit=1).id
-        elif cfop.code in ['5902', '6902'] and not cfop.tax_classification_id:
-            cfop.tax_classification_id = Tax.search([('code', '=', '410999')], limit=1).id
-        elif cfop.code in ['6109'] and not cfop.tax_classification_id:
-            cfop.tax_classification_id = Tax.search([('code', '=', '200022')], limit=1).id
-        _logger.info(
-            "Updated CFOP %s to use tax classification",
-            cfop.code
-        )
+    # cfop_to_change = [5901, 6901, 5902, 6902, 5915, 6915]
+    # cfop_records = Cfop.search([('code', 'in', cfop_to_change)])
+    # for cfop in cfop_records:
+    #     if cfop.code in ['5901', '6901', '5915', '6915'] and not cfop.tax_classification_id:
+    #         cfop.tax_classification_id = Tax.search([('code', '=', '000000')], limit=1).id
+    #     elif cfop.code in ['5902', '6902'] and not cfop.tax_classification_id:
+    #         cfop.tax_classification_id = Tax.search([('code', '=', '410999')], limit=1).id
+    #     elif cfop.code in ['6109'] and not cfop.tax_classification_id:
+    #         cfop.tax_classification_id = Tax.search([('code', '=', '200022')], limit=1).id
+    #     _logger.info(
+    #         "Updated CFOP %s to use tax classification",
+    #         cfop.code
+    #     )

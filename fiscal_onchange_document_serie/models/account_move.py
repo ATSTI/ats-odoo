e3fd9a461638c34be53e1313430149ba3e9f4b2a
OtavioAndretta <otavio12257@gmail.com>
2026-08-21 10:52:28 -0300

    Usando funcao original para recalcular document_number e puxar serie correta

--- fiscal_onchange_document_serie/models/account_move.py
+++ fiscal_onchange_document_serie/models/account_move.py
@@ -1,16 +1,25 @@
-from odoo import models, fields, api
+from odoo import models, api
+from odoo.addons.l10n_br_fiscal.models.document_mixin import FiscalDocumentMixin
+
 
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    @api.onchange("document_type")
-    def _onchange_document_type(self):
+    @api.onchange("document_type_id")
+    def _onchange_document_type_id(self):
         for rec in self:
-            if rec.document_type_id:
-                domain = self.env['l10n_br_fiscal.document.serie'].search([
-                    ('document_type_id', '=', rec.document_type_id.id),
-                    ('active', '=', True)
-                ])
-                if domain:
-                    rec.document_serie_id = domain[0]
-                    rec.document_number = False
+            if not rec.document_type_id:
+                continue
+
+            serie = self.env["l10n_br_fiscal.document.serie"].search([
+                ("document_type_id", "=", rec.document_type_id.id),
+                ("active", "=", True),
+            ], limit=1)
+
+            if serie and serie.company_id == rec.company_id:
+                rec.document_serie_id = False
+                rec.document_number = False
+
+        res = FiscalDocumentMixin._onchange_document_type_id(self)
+        # self._onchange_payment_mode_id()
+        return res
